9ab99deaa9f016e0f7609f3beabdf1ef92a37d2e
Xavier <Xaviermeseguer@ginebro.cat>
2020-05-11 15:32:48 +0200

    afegir crearHospital al main.js

--- js/main.js
+++ js/main.js
@@ -72,7 +72,7 @@ function comprovaCampNumero(objecteRebut) {
                   break;
 
       }
-      // if (isNaN(objecteRebut.value) || (objecteRebut.value=="")) 
+      // if (isNaN(objecteRebut.value) || (objecteRebut.value==""))
       //   {
       //     alert("Must input numbers");
       //     return false;
@@ -134,7 +134,88 @@ function amagaBotons() {
 }
 
 
-/* https://developer.mozilla.org/en-US/docs/Web/API/Element/classList 
+function crearHospital() {
+var nom = document.getElementById("nomHospital").value.toString();
+var maximPacients = parseInt(document.getElementById("maximPacientsHospital").value);
+
+if (nom !== "" && maximPacients > 0) {
+    eleID_h1Titol.innerHTML="<h2>Gestió de l'hospital</h2>" +
+        "<h1><b>" + nom + "</b></h1>";
+    hospital = new Hospital(nom, maximPacients);
+    eleID_divHospital.classList.toggle("d-none");
+
+
+    var cadenaFilaPacient_1,cadenaFilaPacient_2,cadenaFilaPacient_3;
+ /* Creació de tres cadenes cadenaFilaPacient_1, cadenaFilaPacient_2 i cadenaFilaPacient_3
+ ** per faciliar la creació dels <div class="row"> que es crea per a cada pacient.
+ ** */
+
+    var objSelect;
+ /* Creació d'un objecte llisa desplegable, que ompliré amb tots els elements
+ ** de la matriu que acabo de crear llistaMalalties
+ ** */
+
+  objSelect=`<option value=\"${etCapMalaltia}\">------ Escull un malaltia ------</option>`;
+  for (var indexMalaltia=0; indexMalaltia<llistaMalalties.length; indexMalaltia++) {
+      objSelect+=`<option value=\"${indexMalaltia.toString()}\">${llistaMalalties[indexMalaltia]}</option>`;
+  }
+  objSelect+='</select>';
+
+  var htmlINICIDivClassRow = '<div class="row">';
+  var htmlFINALDivClassRow =  '</div> <!-- <div class="row"> -->';
+
+  var htmlINICIDivClassCol = '<div class="col mb-3">';
+  var htmlFINALDivClassCol = '</div> <!-- <div class="col mb-3"> -->';
+
+
+
+  var htmlINICIInputNomPacient = `<input type=\"text\" title=\"Entra el nom del pacient!\" name=\"Nom Pacient\"
+                                onblur=\"validaQueNoEsBuit(this)\" placeHolder=\"Nom pacient\" id=\"nomPacient`;
+
+  var htmlINICIInputSelect = '<select id="malaltia';
+  var htmlFINALClassFormCtrl = '" class="form-control" required minlength="1" maxlength="100">';
+
+    for (var pacient = 0; pacient < maximPacients; pacient++) {
+      var cadPacient = pacient.toString();
+
+      var htmlLabelNomPacient = `<label for=\"nomPacient${pacient}\" class=\"font-weight-bold\">Nom pacient:</label>`;
+      var htmlLabelNomMalaltia = `<label for=\"nomMalaltiaPacient${pacient}\"  class=\"font-weight-bold\">Malaltia de pacient:</label>`;
+
+      document.getElementById("dadesPacient").innerHTML += ( '' +
+        htmlINICIDivClassRow +
+          htmlINICIDivClassCol +
+              htmlLabelNomPacient +
+                htmlINICIInputNomPacient + cadPacient + htmlFINALClassFormCtrl +
+            htmlFINALDivClassCol +
+            htmlINICIDivClassCol +
+              htmlLabelNomMalaltia +
+                htmlINICIInputSelect + cadPacient + htmlFINALClassFormCtrl +
+                  objSelect +
+            htmlFINALDivClassCol +
+          htmlFINALDivClassRow);
+    }
+    eleID_divPacients.classList.toggle("d-none");
+} else {
+  //barra_missatges
+  eleID_barra_missatges.classList.toggle("d-none");
+  if (nom !== "") {
+      eleID_a_text_missatge.innerText = "Cal que entris un nombre al camp màxim pacients!";
+  } else if(maximPacients > 0){
+      eleID_a_text_missatge.innerText = "Cal que entris un nom al camp Hospital!";
+  } else {
+      eleID_a_text_missatge.innerText = "Cal que entris un nom al camp Hospital i un nombre al camp màxim pacients!";
+  }
+
+}
+}
+
+
+
+
+
+
+
+/* https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
 document.getElementById("MyElement").classList.add('MyClass');
 
 document.getElementById("MyElement").classList.remove('MyClass');
@@ -157,7 +238,7 @@ function conteClass(element, nomClass) {
 
 function eliminaClass(elementRebut, nomClass) {
       var arrLlistaClasses;
-      
+
       arrLlistaClasses = elementRebut.className.split(" ");
       if (arrLlistaClasses.indexOf(nomClass) == -1) {  // ens indica que no existeix
             elementRebut.className += " " + nomClass;
@@ -166,7 +247,7 @@ function eliminaClass(elementRebut, nomClass) {
 
     function afegirClass(elementRebut, nomClass) {
       var arrLlistaClasses;
-      
+
       arrLlistaClasses = elementRebut.className.split(" ");
       if (arrLlistaClasses.indexOf(nomClass) == -1) {  // ens indica que no existeix
             elementRebut.className += " " + nomClass;
@@ -174,7 +255,7 @@ function eliminaClass(elementRebut, nomClass) {
     }
 
 
-    
+
     function mostraGestioApp(objecte){
       eleID_divPresentacio.classList.toggle("d-none");
       eleID_divControls.classList.toggle("d-none");
@@ -189,7 +270,7 @@ function eliminaClass(elementRebut, nomClass) {
     function mostraGestioMalaties(objecte){
        mostraMissatge(0);
     }
-    
+
 
 
 function mostraGestioHospital(objecte){
@@ -229,4 +310,3 @@ function ocultaGestioPacients(objecteRebut){
       eleID_divControls.classList.toggle("d-none");
       mostraBotons();
    }
-   
